9b0d147cc0a7f373080f4427cc7988b892166aa8
thereIsNoSpoon8891 <130624304+thereIsNoSpoon8891@users.noreply.github.com>
2023-06-21 15:29:21 -0700

    added CSS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-//
+
 // GET data from
  function getData(){
     axios.get("https://api.vschool.io/shane88/todo")
@@ -12,37 +12,42 @@ function listData(data){
     clearList()
 
 for (i = 0; i < data.length; i++){// loop through the array of objects and append them to the DOM
+    
+    const ulList = document.createElement('ul')
+    document.getElementById('displayitem').appendChild(ulList)
     //display title
     const listTitle = document.createElement('li')
     listTitle.innerHTML = data[i].title
-    document.getElementById('displayitem').appendChild(listTitle)
+    ulList.appendChild(listTitle)
     //price
     const listPrice = document.createElement('li')
     listPrice.textContent = data[i].price
-    document.getElementById('displayitem').appendChild(listPrice)
+    ulList.appendChild(listPrice)
     // description
     const listDescription = document.createElement('li')
     listDescription.textContent = data[i].description
-    document.getElementById('displayitem').appendChild(listDescription)
+    ulList.appendChild(listDescription)
     // display image
     const listImg = document.createElement('li')
     const showImg = document.createElement('img')
     showImg.setAttribute('src', data[i].imgUrl) 
-    document.getElementById('displayitem').appendChild(listImg)
+    ulList.appendChild(listImg)
     listImg.appendChild(showImg)
     // create delete button for todo
-    const ul = document.getElementById("displayitem")
+    
     const li = document.createElement('li')
     const button = document.createElement('button')
-    button.textContent = "Delete this Todo"
+    button.textContent = "Delete"
     li.appendChild(button)
-    ul.appendChild(li) 
+    ulList.appendChild(li) 
     button.setAttribute('id', data[i]._id)
+    button.setAttribute('class', 'deletebutton')
     //create striked list for completed Todos
     if (data[i].completed){// displays text with strike if todo completed: true
-        listTitle.innerHTML = `<s>${data[i].title}<s/>`
-        listPrice.innerHTML = `<s>${data[i].price}<s/>`
-        listDescription.innerHTML = `<s>${data[i].description}<s/>`
+        listTitle.innerHTML = `<s class="complete">${data[i].title}<s/>`
+        listPrice.innerHTML = `<s class="complete">${data[i].price}<s/>`
+        listDescription.innerHTML = `<s class="complete">${data[i].description}<s/>`
+        
     }
     
     const checkBox = document.createElement('input')
@@ -53,17 +58,17 @@ for (i = 0; i < data.length; i++){// loop through the array of objects and appen
     const checkBoxText = document.createElement('label')
     checkBoxText.setAttribute('for', 'putlist')
     checkBoxText.textContent = 'Mark this Todo complete'
-    
-    document.getElementById('displayitem').appendChild(checkBoxText)
-    document.getElementById('displayitem').appendChild(checkBox)
+    checkBoxText.setAttribute('class', 'checkboxes')
+    ulList.appendChild(checkBoxText)
+    ulList.appendChild(checkBox)
     
     const breakText = document.createElement('br')// add break to put element at bottom of list
-    document.getElementById('displayitem').appendChild(breakText)
+    ulList.appendChild(breakText)
     
     const editButton = document.createElement('button')
     editButton.textContent = `Edit`
     editButton.setAttribute('id', data[i]._id + "0")
-    document.getElementById('displayitem').appendChild(editButton)
+    ulList.appendChild(editButton)
     
     const removeButton = document.getElementById(data[i]._id)
     
